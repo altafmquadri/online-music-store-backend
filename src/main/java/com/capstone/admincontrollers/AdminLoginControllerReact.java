3360697374fd5f3d87e52084627e1527e78a3814
Altaf Quadri <altafmquadri@gmail.com>
2021-02-02 14:15:56 -0500

    add songs route in controller

--- src/main/java/com/capstone/admincontrollers/AdminLoginControllerReact.java
+++ src/main/java/com/capstone/admincontrollers/AdminLoginControllerReact.java
@@ -1,15 +1,20 @@
 package com.capstone.admincontrollers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.dao.AdminDAO;
+import com.capstone.dao.SongDAO;
 import com.capstone.model.Admin;
 import com.capstone.model.HelloWorld;
+import com.capstone.model.Song;
 import com.capstone.service.Authentication;
 
 @RestController
@@ -21,6 +26,8 @@ public class AdminLoginControllerReact {
 	AdminDAO adminDao;
 	@Autowired
 	Authentication auth;
+	@Autowired
+	SongDAO songDao;
 	
 	//test mapping
 	@RequestMapping( path="/hello-world") 
@@ -32,6 +39,11 @@ public class AdminLoginControllerReact {
 		public boolean verifyLogin(@RequestBody Admin admin) {
 		boolean isValid = auth.authenticate(admin.getUsername(), admin.getPassword(), adminDao);					
 		return isValid;
+	}
+	
+	@GetMapping("/admin/songs")
+	public List<Song> getSongs(){
+		return (List<Song>) songDao.findAll();
 	}
 
 }
