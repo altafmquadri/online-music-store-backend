8c826ce51cef1f124223d9d3984e97481ad1fc7e
Altaf Quadri <altafmquadri@gmail.com>
2021-02-04 01:39:01 -0500

    changed to only permit authenticate

--- src/main/java/com/capstone/config/WebSecurityConfig.java
+++ src/main/java/com/capstone/config/WebSecurityConfig.java
@@ -52,11 +52,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
-				// dont authenticate this particular request
+				// don't authenticate this particular request
 				.authorizeRequests().antMatchers("/api/authenticate").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
-				// make sure we use stateless session; session won't be used to
+				// make sure we use state-less session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
