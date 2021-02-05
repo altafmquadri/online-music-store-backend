f83faee8a42651ffba4a0ce9292841acdaa698a5
Altaf Quadri <altafmquadri@gmail.com>
2021-02-05 13:15:52 -0500

    adding cors method to bypass cors

--- src/main/java/com/capstone/config/WebSecurityConfig.java
+++ src/main/java/com/capstone/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.cors();
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				
